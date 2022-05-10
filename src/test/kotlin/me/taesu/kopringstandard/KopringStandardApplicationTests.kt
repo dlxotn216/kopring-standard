53550d116f654f149de355abb09f97f1da092c4c
Lee Tae Su <taesu@crscube.co.kr>
2022-05-10 17:08:51 +0900

    Initial Commit

---
+++ src/test/kotlin/me/taesu/kopringstandard/KopringStandardApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.taesu.kopringstandard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringStandardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
